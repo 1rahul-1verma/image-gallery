634cadd44ea733a296cb8642f06763bab13d5d5a
1rahul-1verma <30508582+1rahul-1verma@users.noreply.github.com>
2021-07-07 16:50:57 +0530

    Add files via upload

--- src/controller/displayController.js
+++ src/controller/displayController.js
@@ -1,20 +1,6 @@
-import { imgInfo } from "../resources/imageInfo.js" ;
 
-function updateDisplay(indx) {
-    updateDisplayCaption(indx);
-    updateDisplayImage(indx);
+function eventDisplay(){
+
 }
 
-function updateDisplayImage(indx) {
-    console.log(indx);
-    let displayImage = document.querySelector("#display-image");
-    displayImage.src = imgInfo[indx].src;
-}
-
-function updateDisplayCaption(indx) {
-    let displayCaption = document.querySelector("#display-caption");
-    displayCaption.textContent = imgInfo[indx].caption;
-}
-
-
-export { updateDisplay };
+export { eventDisplay };
